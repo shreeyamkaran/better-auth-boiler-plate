de146e183be5e71883fec8678a4a54ddf864e385
Shreeyam Karan <karanshreeyam@gmail.com>
2025-05-23 18:36:48 +0530

    Add table and sort users

--- app/admin/dashboard/manage-users/page.tsx
+++ app/admin/dashboard/manage-users/page.tsx
@@ -1,15 +1,67 @@
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { prisma } from '@/lib/prisma';
+import { Trash } from 'lucide-react';
 import React from 'react';
 
+const rolePriority = {
+  ADMIN: 0,
+  USER: 1,
+  GUEST: 2,
+};
+
 export default async function AdminDashboard() {
-  const users = await prisma.user.findMany({
-    orderBy: {
-      name: 'asc',
-    },
+  const users = (await prisma.user.findMany()).sort((a, b) => {
+    const roleDiff = rolePriority[a.role] - rolePriority[b.role];
+    if (roleDiff !== 0) return roleDiff;
+    return a.name.localeCompare(b.name);
   });
   return (
-    <div>
-      {users && users.map(user => <pre key={user.id}>{JSON.stringify(user, null, 2)}</pre>)}
-    </div>
+    <>
+      <div className="mb-8 text-2xl sm:text-4xl md:text-5xl lg:text-6xl">Manage Users</div>
+      <Table className="table-fixed">
+        <TableBody>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+          {users &&
+            users.map(user => (
+              <TableRow key={user.id}>
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <TableCell className="truncate max-w-[200px] overflow-hidden whitespace-nowrap cursor-help">
+                        {user.id}
+                      </TableCell>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{user.id}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                  <TableCell className="truncate max-w-[200px] overflow-hidden whitespace-nowrap">
+                    {user.name}
+                  </TableCell>
+                  <TableCell className="truncate max-w-[200px] overflow-hidden whitespace-nowrap">
+                    {user.email}
+                  </TableCell>
+                  <TableCell className="truncate max-w-[200px] overflow-hidden whitespace-nowrap">
+                    {user.role}
+                  </TableCell>
+                  <TableCell className="truncate max-w-[200px] overflow-hidden whitespace-nowrap">
+                    <Button>
+                      <Trash />
+                    </Button>
+                  </TableCell>
+                </TooltipProvider>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
